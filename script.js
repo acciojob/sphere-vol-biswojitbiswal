052b12b4912fe1ed47660778917f04810ec8b623
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-25 11:55:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault();
+
+	const radiusInput = document.getElementById("radius");
+	const radius = parseFloat(radiusInput.value);
+	
+	const volume = document.getElementById("volume");
+
+	if(isNaN(radius) && radius < 0){
+		volume.value = 'NaN';
+		return;
+	}
+
+	const result = (4 / 3) * Math.PI * Math.pow(radius, 3);
+	
+	volume.value = result.toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
